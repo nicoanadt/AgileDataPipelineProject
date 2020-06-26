29b0b68bda364687a468873aba4348998953b741
Nico Anandito <nico.anandito@gmail.com>
2020-06-26 12:16:30 +0100

    Update spark docker deployment to connect with HDFS

--- docker-spark/deployment/start_cmd.sh
+++ docker-spark/deployment/start_cmd.sh
@@ -3,6 +3,12 @@
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export SPARK_HOME=/usr/local/spark
 PATH=$PATH:/usr/local/spark/bin
+export HADOOP_HOME="/opt/hadoop"
+export HADOOP_CONF_DIR="/opt/hadoop/etc/hadoop"
+export PATH=$HADOOP_HOME/bin:$PATH
+
+cat /dev/zero | ssh-keygen -q -t rsa -N ""
+cat  ~/.ssh/id_rsa.pub | sshpass -proot ssh root@myhdfs 'cat >> .ssh/authorized_keys'
 
 /usr/sbin/sshd
 /usr/local/spark/sbin/start-master.sh 2>&1
---
+++ docker-spark/hdfs/hadoop-env.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+export HADOOP_HOME="/opt/hadoop"
+export HADOOP_CONF_DIR="/opt/hadoop/etc/hadoop"
+export PATH=$HADOOP_HOME/bin:$PATH
